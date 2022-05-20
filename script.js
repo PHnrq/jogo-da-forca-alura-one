dbc3959a0ba25d9144ef492bbdb735e6ae15a761
PHnrq <paulofigueredo7@gmail.com>
2022-05-20 15:45:55 -0300

    Refatoração do codigo

--- script.js
+++ script.js
@@ -1,235 +1,210 @@
-let wordsForGame = [
-    {nome: 'Cachorro', categoria: 'Animal'},
-    {nome: 'Gato', categoria: 'Animal'},
-    {nome: 'Cavalo', categoria: 'Animal'},
-    {nome: 'Elefante', categoria: 'Animal'},
-    {nome: 'Leao', categoria: 'Animal'},
-    {nome: 'Macaco', categoria: 'Animal'},
-    {nome: 'Pato', categoria: 'Animal'},
-    {nome: 'Porco', categoria: 'Animal'},
-    {nome: 'Rato', categoria: 'Animal'},
-    {nome: 'Tigre', categoria: 'Animal'},
-    {nome: 'Vaca', categoria: 'Animal'},
-    {nome: 'Abacate', categoria: 'Fruta'},
-    {nome: 'Abacaxi', categoria: 'Fruta'},
-    {nome: 'Acai', categoria: 'Fruta'},
-    {nome: 'Ameixa', categoria: 'Fruta'},
-    {nome: 'Banana', categoria: 'Fruta'},
-    {nome: 'Caju', categoria: 'Fruta'},
-    {nome: 'Cereja', categoria: 'Fruta'},
-    {nome: 'Coco', categoria: 'Fruta'},
-    {nome: 'Damasco', categoria: 'Fruta'},
-    {nome: 'Goiaba', categoria: 'Fruta'},
-    {nome: 'Jaca', categoria: 'Fruta'},
-    {nome: 'Jambo', categoria: 'Fruta'},
-    {nome: 'Jujuba', categoria: 'Fruta'},
-    {nome: 'Kiwi', categoria: 'Fruta'},
-    {nome: 'Laranja', categoria: 'Fruta'},
-    {nome: 'Limao', categoria: 'Fruta'},
-    {nome: 'Lima', categoria: 'Fruta'},
-    {nome: 'Manga', categoria: 'Fruta'},
-    {nome: 'Maracuja', categoria: 'Fruta'},
-    {nome: 'Melancia', categoria: 'Fruta'},
-    {nome: 'Melao', categoria: 'Fruta'}]
+//Lista da palavras
+let gameWords = [
+  { name: "Cachorro", category: "Animal" },
+  { name: "Gato", category: "Animal" },
+  { name: "Cavalo", category: "Animal" },
+  { name: "Elefante", category: "Animal" },
+  { name: "Leao", category: "Animal" },
+  { name: "Macaco", category: "Animal" },
+  { name: "Pato", category: "Animal" },
+  { name: "Porco", category: "Animal" },
+  { name: "Rato", category: "Animal" },
+  { name: "Tigre", category: "Animal" },
+  { name: "Vaca", category: "Animal" },
+  { name: "Abacate", category: "Fruta" },
+  { name: "Abacaxi", category: "Fruta" },
+  { name: "Acai", category: "Fruta" },
+  { name: "Ameixa", category: "Fruta" },
+  { name: "Banana", category: "Fruta" },
+  { name: "Caju", category: "Fruta" },
+  { name: "Cereja", category: "Fruta" },
+  { name: "Coco", category: "Fruta" },
+  { name: "Damasco", category: "Fruta" },
+  { name: "Goiaba", category: "Fruta" },
+  { name: "Jaca", category: "Fruta" },
+  { name: "Jambo", category: "Fruta" },
+  { name: "Kiwi", category: "Fruta" },
+  { name: "Laranja", category: "Fruta" },
+  { name: "Limao", category: "Fruta" },
+  { name: "Lima", category: "Fruta" },
+  { name: "Manga", category: "Fruta" },
+  { name: "Maracuja", category: "Fruta" },
+  { name: "Melancia", category: "Fruta" },
+  { name: "Melao", category: "Fruta" },
+];
 
-const body = document.querySelector('body');
-const input = document.querySelector('input');
-const spanWrongLetters = document.querySelector('#wrongLetters');
-const spanDica = document.querySelector('#dica');
-const inputNewWord = document.querySelector('#newWord');
-const inputCategory = document.querySelector('#category');
-const winnerModal = document.querySelector('#winnerModal');
-const looseModal = document.querySelector('#looseModal');
-const looserMsg = document.querySelector('#looserMsg');
-const score = document.querySelector('#score');
+//elementos do game.html
+const body = document.querySelector("body");
+const input = document.querySelector("input");
+const spanWrongLetters = document.querySelector("#wrongLetters");
+const spanTip = document.querySelector("#tip");
+const winnerModal = document.querySelector("#winnerModal");
+const looseModal = document.querySelector("#looseModal");
+const looserMsg = document.querySelector("#looserMsg");
+const score = document.querySelector("#score");
+//elementos do new-word.html
+const inputNewWord = document.querySelector("#newWord");
+const inputCategory = document.querySelector("#category");
 
-let wordDrawn = '';
-let letterTry = [];
-let wrongLetters = [];
+//Variaveis globais
+let wordDrawn = "";
+let currentLetters = "";
 let rightLetters = 0;
-let erros = 0;
-let currentLetters = '';
 let updateScore = 0;
+let erros = 0;
+let attemptsLetters = [];
+let wrongLetters = [];
 
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-ctx.fillStyle = '#0A3871';
-ctx.strokeStyle = '#0A3871';
+//Configurações do canvas
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+ctx.fillStyle = "#0A3871";
+ctx.strokeStyle = "#0A3871";
 ctx.lineWidth = 4;
-ctx.font =  "24px Inter";
-
-
+ctx.font = "24px Inter";
 
 function addWord() {
-    const newWordObject = {nome: inputNewWord.value, categoria: inputCategory.value};
-    console.log(newWordObject);
-    wordsForGame.push(newWordObject);
-    
-    window.location.replace("./game.html");
+  const newWord = { name: inputNewWord.value, category: inputCategory.value };
+  gameWords.push(newWord);
+
+  window.location.replace("./game.html"); //Direciona para o game.html
 }
 
-function setRandomWord(){
-    const randomObject = wordsForGame[Math.floor(Math.random() * wordsForGame.length)];
-    const randomWord = randomObject.nome;
-    const randomCategory = randomObject.categoria;
-    spanDica.innerHTML = `${randomCategory}`;
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    wordDrawn = randomWord.toUpperCase();
-    categoryDrawn = randomCategory.toUpperCase();
-    winnerModal.classList.add('hidden');
-    looseModal.classList.add('hidden');
-    erros = 0;
-    letterTry = [];
-    wrongLetters = [];
-    currentLetters = null;
-    rightLetters = 0;
-    spanWrongLetters.innerHTML = '';
-
-    desenhaTraços();
-    
-}
-
-function drawGibbet(){
-    switch(erros){
-        case 1:
-            dranwGibbet()
-            break;
-
-        case 2:
-            drawHead();
-            break;
-        
-        case 3:
-            drawBody();
-            break;
-        
-        case 4:
-            drawLeftArm();
-            break;
-        
-        case 5:
-            drawRightArm();
-            break;
-        
-        case 6:
-            drawLeftLeg();
-            break;
-        
-        case 7:
-            drawRightLeg();
-            break;
-
-        default:
-            break;
-    }
-}
-
-function dranwGibbet(){
-    ctx.fillRect(0, 300, canvas.width, 5); // Primeira barra
-    ctx.fillRect(80, 50, 5, 250);  // segunda barra
-    ctx.fillRect(80, 50, 173, 5); // terceira barra
-    ctx.fillRect(253, 50, 5, 48) // quarta barra
-}
-
-function drawHead(){
-    ctx.beginPath();
-    ctx.arc(255, 127, 30, 0, 2 * Math.PI); // Cabeça
-    ctx.stroke();
-}
-
-function drawBody(){
-    ctx.fillRect(252, 155, 4, 90); //Corpo
-}
-
-function drawLeftArm(){
-    ctx.beginPath(); //Braço esquerdo
-    ctx.moveTo(250, 157);
-    ctx.lineTo(230, 200);
-    ctx.stroke();
-}
-
-function drawRightArm(){
-    ctx.beginPath(); //Braço direito
-    ctx.moveTo(258, 157);
-    ctx.lineTo(280, 200);
-    ctx.stroke();
-}
-
-function drawLeftLeg(){
-    ctx.beginPath(); //Perna esquerdo
-    ctx.moveTo(254, 242);
-    ctx.lineTo(230, 285);
-    ctx.stroke();
-}
-
-function drawRightLeg(){
-    ctx.beginPath(); //Perna direito
-    ctx.moveTo(254, 242);
-    ctx.lineTo(280, 285);
-    ctx.stroke();
-}
-
-input.addEventListener('input', function(e){
-
-    let arr = e.target.value.split('');
-    let lastLetter = arr[arr.length - 1];
-
-    if(letterTry.find(letter => letter == lastLetter)){
-        return;
-    }else{
-        letterTry.push(lastLetter);
-        currentLetters = lastLetter.toUpperCase();
-        checkWrongLetters();
-        desenhaTraços();
-    }
-
-    spanWrongLetters.innerHTML = wrongLetters.join('');
-})
-
-
-body.addEventListener('click', function(e){
-    input.focus();
-})
-
-function desenhaTraços(){
+function drawLinesAndWords() {
   input.focus();
-  score.innerHTML = `Pontuação: ${updateScore}`;   
-  
+  score.innerHTML = `Pontuação: ${updateScore}`; //Mostra a pontuação na tela
 
-  let linhaInicial = (canvas.width - (35 * wordDrawn.length)+30) / 2;
+  let initialPosition = (canvas.width - 35 * wordDrawn.length + 30) / 2;
 
-  for(let i = 0; i < wordDrawn.length; i++){
-    ctx.fillRect(linhaInicial,370 , 24, 4); //Traços
+  for (let i = 0; i < wordDrawn.length; i++) {
+    ctx.fillRect(initialPosition, 370, 24, 4);
 
-    if(currentLetters == wordDrawn[i]){
-        ctx.fillText(wordDrawn[i], linhaInicial, 360);
-        rightLetters++;
+    if (currentLetters == wordDrawn[i]) {
+      ctx.fillText(wordDrawn[i], initialPosition, 360);
+      rightLetters++;
     }
-    linhaInicial += (24 + 8);
+    initialPosition += 24 + 8;
   }
 
-  if(rightLetters == wordDrawn.length){
-    setTimeout(function(){
-        updateScore += 10
-        score.innerHTML = `Pontuação: ${updateScore}`;
-        winnerModal.classList.remove('hidden');
-    }, 50)
+  isLetterRight();
+  isLetterWrong();
+}
+
+function isLetterRight() {
+  if (rightLetters == wordDrawn.length) {
+    setTimeout(function () {
+      updateScore += 10;
+      score.innerHTML = `Pontuação: ${updateScore}`;
+      winnerModal.classList.remove("hidden");
+    }, 50);
   }
 }
 
-function checkWrongLetters(){
-  if(wordDrawn.indexOf(currentLetters) == -1){
+function isLetterWrong() {
+  if (wordDrawn.indexOf(currentLetters) == -1) {
     wrongLetters.push(currentLetters);
     erros++;
     drawGibbet();
   }
 
-  if(erros === 7){
-    setTimeout(function(){
-      updateScore = 0
+  if (erros === 7) {
+    setTimeout(function () {
+      updateScore = 0;
       looserMsg.innerHTML = `Você errou, a palavra era: <strong>${wordDrawn}</strong>`;
-      looseModal.classList.remove('hidden');
-    }, 50)
+      looseModal.classList.remove("hidden");
+    }, 50);
   }
 }
+
+function setRandomWord() {
+  const randomObject = gameWords[Math.floor(Math.random() * gameWords.length)]; //Pega um objeto aleatorio da lista
+  const randomWord = randomObject.name; //Pega a palavra do objeto
+  const randomCategory = randomObject.category; //Pega a categoria do objeto
+  spanTip.innerHTML = `${randomCategory}`; //Mostra a categoria na tela
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height); //Limpa o Canvas
+  wordDrawn = randomWord.toUpperCase(); //Pega a palavra e transforma em maiuscula
+  categoryDrawn = randomCategory.toUpperCase(); //Pega a categoria e transforma em maiuscula
+  winnerModal.classList.add("hidden"); //Esconde o modal de vitória
+  looseModal.classList.add("hidden"); //Esconde o modal de derrota
+  erros = 0; //Zera o contador de erros
+  attemptsLetters = []; //Zera a lista de letras que já foram tentadas
+  wrongLetters = []; //Zera a lista de letras erradas
+  currentLetters = ""; //zera a letra atual
+  rightLetters = 0; //Zera o contador de letras certas
+  spanWrongLetters.innerHTML = ""; //Limpa da tela a lista de letras erradas
+
+  drawLinesAndWords();
+}
+
+function drawGibbet() {
+  switch (erros) {
+    case 1: //Desenha a forca
+      ctx.fillRect(0, 300, canvas.width, 5); // Primeira btypedLettersa
+      ctx.fillRect(80, 50, 5, 250); // segunda btypedLettersa
+      ctx.fillRect(80, 50, 173, 5); // terceira btypedLettersa
+      ctx.fillRect(253, 50, 5, 48); // quarta btypedLettersa
+      break;
+
+    case 2: //Desenha a cabeça
+      ctx.beginPath();
+      ctx.arc(255, 127, 30, 0, 2 * Math.PI);
+      ctx.stroke();
+      break;
+
+    case 3: //Desenha o corpo
+      ctx.fillRect(252, 155, 4, 90);
+      break;
+
+    case 4: //Desenha braço esquerdo
+      ctx.beginPath();
+      ctx.moveTo(250, 157);
+      ctx.lineTo(230, 200);
+      ctx.stroke();
+      break;
+
+    case 5: //Desenha braço direito
+      ctx.beginPath();
+      ctx.moveTo(258, 157);
+      ctx.lineTo(280, 200);
+      ctx.stroke();
+      break;
+
+    case 6: //Desenha perna esquerda
+      ctx.beginPath();
+      ctx.moveTo(254, 242);
+      ctx.lineTo(230, 285);
+      ctx.stroke();
+      break;
+
+    case 7: //Desenhada perna direita
+      ctx.beginPath();
+      ctx.moveTo(254, 242);
+      ctx.lineTo(280, 285);
+      ctx.stroke();
+      break;
+
+    default:
+      break;
+  }
+}
+
+body.addEventListener("click", function (e) {
+  input.focus(); //Foca o input ao clicar no body, facilitando o uso do teclado mobile
+});
+
+input.addEventListener("input", function (e) {
+  let typedLetters = e.target.value.split(""); //Retorna um array com as letras digitadas
+  let lastLetter = typedLetters[typedLetters.length - 1]; //Pega a ultima letra digitada
+
+  if (attemptsLetters.find((letter) => letter === lastLetter)) {
+    return; //Se a letra já foi tentada, não faz nada
+  } else {
+    //Se a letra não foi tentada, adiciona na lista de tentativas
+    attemptsLetters.push(lastLetter); //Adiciona a letra na lista de tentativas
+    currentLetters = lastLetter.toUpperCase(); //Transforma a letra atual em maiuscula
+    drawLinesAndWords();
+  }
+
+  spanWrongLetters.innerHTML = wrongLetters.join(""); //Mostra as letras erradas na tela
+});
